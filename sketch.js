e40c755b96c45f17baffe4b9dc39ff2c03ac15f1
Coldaewon <deawon04072@gmail.com>
2024-12-08 23:42:23 +0900

    파티클 클래스 생성

--- sketch.js
+++ sketch.js
@@ -6,3 +6,27 @@ function setup() {
 function draw() {
   background(220);
 }
+
+class Particle {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(random(-2, 2), random(-2, 2));
+    this.acc = createVector(0, 0);
+  }
+  
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  display() {
+    fill(255);
+    noStroke();
+    ellipse(this.pos.x, this.pos.y, 5);
+  }
+}
