ad393c3964727849fb1207e2bad5b40126ac2587
Coldaewon <deawon04072@gmail.com>
2024-12-15 17:27:33 +0900

    오류 발생

--- sketch.js
+++ sketch.js
@@ -27,59 +27,27 @@ function mousePressed() {
   particles.push(new Particle(mouseX, mouseY));
 }
 
-class Particle {
-  constructor(x, y) {
-    this.pos = createVector(x, y);
-    this.vel = createVector(random(-2, 2), random(-2, 2)); 
-    this.acc = createVector(0, 0);
-    this.isOrbiting = false; 
+class ParticleSystem{
+  constructor(x,y){
+    this.center = createVector(x, y);
+    this.particles[];
+
   }
 
-  applyForce(force) {
-    this.acc.add(force);
+  addParticle(x, y){
+  this.particles.push(new Particle(x, y));
   }
 
- 
-  applyGravity(target) {
-    let dir = p5.Vector.sub(target, this.pos); 
-    let distance = constrain(dir.mag(), 10, 200); 
-    let strength = 10 / (distance * distance); 
-    dir.setMag(strength);
-    this.applyForce(dir);
+  run(){
+  fill(255, 204, 0);
+  noStroke();
+  ellipse(this.center.x, this.center.y, 20);
   }
 
-
-  applyRepulsion(target) {
-    let dir = p5.Vector.sub(this.pos, target); 
-    let distance = dir.mag();
-    if (distance < 100) { 
-      let strength = 100 / (distance * distance);
-      dir.setMag(strength);
-      this.applyForce(dir);
-    } else if (!this.isOrbiting && distance >= 50) {
-     
-      this.startOrbit(target);
-    }
-  }
-
- 
-  startOrbit(target) {
-    this.isOrbiting = true;
-    let dir = p5.Vector.sub(this.pos, target); 
-    let tangent = dir.copy().rotate(HALF_PI); 
-    tangent.setMag(sqrt(10 / dir.mag())); 
-    this.vel = tangent; 
-  }
-
-  update() {
-    this.vel.add(this.acc); 
-    this.pos.add(this.vel); 
-    this.acc.mult(0); 
-  }
-
-  display() {
-    fill(255);
-    noStroke();
-    ellipse(this.pos.x, this.pos.y, 5);
+  for(let p of this.particles){
+    p.applyGravity(this.center);
+    p.applyRepulsion(this.center);
+    p.update();
+    p.display();
   }
 }
